ba900742116d749154a1d8325429095e6438e6ef
Queiroz-Victor <queirozvictor915@gmail.com>
2026-06-24 21:59:17 +0900

    Preparando deploy

--- react-styles/vite.config.js
+++ react-styles/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-  })],
+  plugins: 
+  [react({jsxImportSource: '@emotion/react',})],
+  base: '/Css-in-Js/'
 })
